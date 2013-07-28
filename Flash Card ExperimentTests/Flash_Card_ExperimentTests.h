c67b0d4f53654c0953272ff0ce2b73c157748bc4
donanque <0017HomeFolder@donners-macbook.local>
2013-07-28 13:39:21 -0700

    Moved around files

---
+++ Flash Card ExperimentTests/Flash_Card_ExperimentTests.h
@@ -0,0 +1,13 @@
+//
+//  Flash_Card_ExperimentTests.h
+//  Flash Card ExperimentTests
+//
+//  Created by Don Anque on 7/26/13.
+//  Copyright (c) 2013 Don Anque. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Flash_Card_ExperimentTests : SenTestCase
+
+@end
---
+++ Flash Card ExperimentTests/Flash_Card_ExperimentTests.m
@@ -0,0 +1,32 @@
+//
+//  Flash_Card_ExperimentTests.m
+//  Flash Card ExperimentTests
+//
+//  Created by Don Anque on 7/26/13.
+//  Copyright (c) 2013 Don Anque. All rights reserved.
+//
+
+#import "Flash_Card_ExperimentTests.h"
+
+@implementation Flash_Card_ExperimentTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Flash Card ExperimentTests");
+}
+
+@end
